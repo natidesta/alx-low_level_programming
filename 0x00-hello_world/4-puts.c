d93eb027be4d3db9cb448602d88c9d5ae5d13f86
natidesta <natidesta21@gmail.com>
2023-06-15 13:20:41 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
-** main - This is a description
-**/
+/**
+ * *main - Entry point
+ * *Return: Always 0 (Success)
+ * */
 int main(void) 
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"");
 	return (0);
 }
