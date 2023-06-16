a51966ac1f960791be6a588b8b4140ea97137c74
natidesta <natidesta21@gmail.com>
2023-06-16 12:20:45 -0700

    prints if number is +ve or -ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,13 @@
 /**
 ** main - Prints a random number and states whether
 **        it is positive, negative, or zero.
-**  
+** 
 ** Return: Always 0.
 **/
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
