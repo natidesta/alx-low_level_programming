016dc2f0a963f135c3c83b27951f1e526b615ca2
natidesta <natidesta21@gmail.com>
2023-06-20 10:20:40 -0700

    print alphabets lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+** main - Prints the alphabets in lowrcase.
+** Return: Always 0.
+**/
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+        	_putchar('c');
+	_putchar('\n');
+        return (0);
+}
+
