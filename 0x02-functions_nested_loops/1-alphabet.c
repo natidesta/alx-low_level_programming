ca4b628c93ccbb53b8fcb48fcc67140c9f4ba72f
natidesta <natidesta21@gmail.com>
2023-06-20 10:32:44 -0700

    print alphabets lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,16 @@
 ** main - Prints the alphabets in lowrcase.
 ** Return: Always 0.
 **/
+
+/**
+* * print_alphabet - prints the alphabet in lowercase.
+* */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
-        	_putchar('c');
+		_putchar(c);
 	_putchar('\n');
 }
 
