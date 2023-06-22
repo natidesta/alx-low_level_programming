5ce33e8d12ecc15e1adfe58e9516312c10bd828e
natidesta <natidesta21@gmail.com>
2023-06-22 10:41:49 -0700

    test +ve or -ve

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,6 @@
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
-* @i
 */
 
 int main(void)
@@ -11,6 +10,7 @@ int main(void)
 	int i;
 	
 	i = 0;
+
 	positive_or_negative(i);
 	return (0);
 }
